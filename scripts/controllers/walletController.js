ba06696234f2aa4aa8398c953ae273aba95fdbd8
gkucmierz <gkucmierz@gmail.com>
2014-06-25 02:21:31 +0200

    first commit

---
+++ scripts/controllers/walletController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+angular.module('walletApp')
+.controller('WalletController', function($scope) {
+
+    $scope.wallet = [
+        '1grzes2zcfyRHcmXDLwnXiEuYBH7eqNVh',
+        '12V3p2W6vPLDDdnbKybpgpUbffiKaCx8Q3',
+        '1FnQT3ozL8gXeySkntA7bVKT4P2BtYZ1Wu'
+    ];
+    
+})
---
+++ scripts/walletApp.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var walletApp = angular.module('walletApp', []);
+
+walletApp.config(function() {
+
+});
