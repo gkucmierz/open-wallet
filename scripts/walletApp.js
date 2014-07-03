82e7bf844dbf0e80035dd554be368ce9a33fb0a3
gkucmierz <gkucmierz@gmail.com>
2014-07-03 21:48:47 +0200

    #19 added i18n files

--- scripts/walletApp.js
+++ scripts/walletApp.js
@@ -3,10 +3,20 @@
 angular.module('walletApp', ['ngStorage']);
 
 angular.module('walletApp')
-.config(function($httpProvider) {
+.config(function($httpProvider, $translateProvider) {
     //Enable cross domain calls
     $httpProvider.defaults.useXDomain = true;
-
     //Remove the header used to identify ajax call  that would prevent CORS from working
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'i18n/locale-',
+        suffix: '.json'
+    });
+
+    // store last language decision in local storage
+    $translateProvider.useLocalStorage();
+
+    $translateProvider.preferredLanguage('en');
 });
