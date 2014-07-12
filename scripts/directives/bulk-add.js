e196c374b90bc84b80df03fe0d7d0a1a04bcb374
gkucmierz <gkucmierz@gmail.com>
2014-07-13 00:37:07 +0200

    #9 first attemp to parse addresses

--- scripts/directives/bulk-add.js
+++ scripts/directives/bulk-add.js
@@ -3,12 +3,30 @@
 angular.module('walletApp').directive('bulkAdd', function (
     PathGeneratorService
 ) {
+    // https://en.bitcoin.it/wiki/Address
+    // A Bitcoin address, or simply address, is an identifier of 27-34 alphanumeric characters, beginning with the number 1 or 3, that represents a possible destination for a Bitcoin payment.
+    var bitcoinAddressRegExp = /[1-3][a-zA-Z0-9]{26,33}/g;
 
     return {
         restrict: 'C',
         templateUrl: PathGeneratorService.getView('wallet', 'bulk-add'),
         scope: {},
-        link: function() {
+        link: function(scope) {
+            scope.foundEntries = [];
+            scope.inputText = '';
+
+            scope.$watch('inputText', function(inputText) {
+                var uniquePotentialAddresses = _.unique(inputText.match(bitcoinAddressRegExp));
+
+                scope.foundEntries = _.map(uniquePotentialAddresses, function(address) {
+                    return {
+                        address: address
+                    };
+                }).slice(0, 25);
+            });
+
+
+            scope.adding = true;
         }
     };
 });
