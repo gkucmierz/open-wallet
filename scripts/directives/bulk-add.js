7fa55346ac572b4a7ff4c0ff61ed6b7fe858f3b2
gkucmierz <gkucmierz@gmail.com>
2014-07-13 02:18:20 +0200

    #9 filter only valid addresses

--- scripts/directives/bulk-add.js
+++ scripts/directives/bulk-add.js
@@ -1,11 +1,17 @@
 'use strict';
 
 angular.module('walletApp').directive('bulkAdd', function (
-    PathGeneratorService
+    PathGeneratorService,
+    BitcoreService
 ) {
     // https://en.bitcoin.it/wiki/Address
     // A Bitcoin address, or simply address, is an identifier of 27-34 alphanumeric characters, beginning with the number 1 or 3, that represents a possible destination for a Bitcoin payment.
     var bitcoinAddressRegExp = /[1-3][a-zA-Z0-9]{26,33}/g;
+
+    var isValidAddress = function(address) {
+        var addr = new BitcoreService.Address(address);
+        return addr.isValid();
+    };
 
     return {
         restrict: 'C',
@@ -18,19 +24,19 @@ angular.module('walletApp').directive('bulkAdd', function (
             scope.$watch('inputText', function(inputText) {
                 var uniquePotentialAddresses = _.unique(inputText.match(bitcoinAddressRegExp));
 
-                scope.foundEntries = _.map(uniquePotentialAddresses, function(address) {
+                var allEntries = _.map(uniquePotentialAddresses, function(address) {
                     return {
                         address: address
                     };
                 });
 
-                // scope.foundEntries = [{
-                //     address: '1J2KcigW48G5HhKrc5NkaNuzCT7KUgukHC'
-                // }];
+                scope.foundEntries = _.filter(allEntries, function(entry) {
+                    return isValidAddress(entry.address);
+                });
             });
 
-
             scope.adding = true;
+
         }
     };
 });
