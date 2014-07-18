9f77955912a1a66e91c299a00a9fe63164b7adeb
gkucmierz <gkucmierz@gmail.com>
2014-07-18 21:58:30 +0200

    #30 made walletDataService working

--- scripts/services/walletDataService.js
+++ scripts/services/walletDataService.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('walletApp').service('WalletDataService', function(
+    StorageService
 ) {
-    // var storageKey = 'wallet';
-    var data = [{address: '1grzes2zcfyRHcmXDLwnXiEuYBH7eqNVh'}];
+    var storageKey = 'wallet';
+    var data = StorageService.default(storageKey, [
+        {address: '1grzes2zcfyRHcmXDLwnXiEuYBH7eqNVh'}
+    ]);
 
     return {
         save: function() {
+            StorageService.set(storageKey, data);
         },
         data: data
     };
