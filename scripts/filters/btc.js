16437a814dfaf5bfda81ddc2838c7e3168b5b84b
gkucmierz <gkucmierz@gmail.com>
2014-07-18 13:52:04 +0200

    moved satoshi -> btc conversion to filter

--- scripts/filters/btc.js
+++ scripts/filters/btc.js
@@ -3,7 +3,7 @@
 angular.module('walletApp')
 .filter('btc', function () {
     return function (input) {
-        var vol = parseFloat(input);
+        var vol = parseInt(input) * 1e-8;
         return isNaN(vol) ? '-' : vol.toFixed(8);
     };
 });
